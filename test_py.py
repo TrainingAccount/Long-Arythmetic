9ebe449f5fbf15f7f1a1552ad9b703a61aaf95f9
TrainingAccount <mj-25@mail.ru>
2013-06-26 23:40:30 +0800

    Update test_py.py

--- test_py.py
+++ test_py.py
@@ -1,28 +1,33 @@
 import sys
 import longnumber_cpp as longnum
 
-first.read(sys.argv[1])
-second.read(sys.argv[3])
+if len(sys.argv) < 4
+	print 'Invalid parameters; try again, friend'
+	
+else
 
-first = longnum.LongNum(sys.argv[1])
-second = longnum.LongNum(sys.argv[3])
+	first.read(sys.argv[1])
+	second.read(sys.argv[3])
 
-selector = sys.argv[2]
-if selector == "+":
-	result = first + second
-elif selector == "-":
-	result = first - second
-elif selector == "*":
-	result = first * second
-elif selector == "/":
-	result = first / second
-elif selector == "%":
-	result = first % second
-elif selector == "^":
-	result = first ^ second
+	first = longnum.LongNum(sys.argv[1])
+	second = longnum.LongNum(sys.argv[3])
+
+	selector = sys.argv[2]
+	if selector == "+":
+		result = first + second
+	elif selector == "-":
+		result = first - second
+	elif selector == "*":	
+		result = first * second
+	elif selector == "/":
+		result = first / second
+	elif selector == "%":
+		result = first % second
+	elif selector == "^":
+		result = first ^ second
  
-if len(sys.argv) > 4:
-	module = longnum.LongNum(sys.argv[5])
-	result = result % mod
+	if len(sys.argv) > 4:
+		module = longnum.LongNum(sys.argv[5])
+		result = result % mod
 
-result.output(sys.argv[4])
+	result.output(sys.argv[4])
