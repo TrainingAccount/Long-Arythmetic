957c08a4255283f4db7fb44a1c7217c23aa45375
TrainingAccount <mj-25@mail.ru>
2013-06-28 00:25:43 +0800

    Update longnumber_cpp.cpp

--- longnumber_cpp.cpp
+++ longnumber_cpp.cpp
@@ -78,12 +78,12 @@ void LongNum::toFile(const char * FileName)
 	writingLongNumberToFile(FileName, Number);
 }
 
-int LongNum::operator ==(const LongNum &second)
+int LongNum::operator ==(LongNum &second)
 {
 	return (parity(Number, second.getNumber()));
 }
 
-int LongNum::operator >(const LongNum &second)
+int LongNum::operator >(LongNum &second)
 {
 	return (more(Number, second.getNumber()));
 }
